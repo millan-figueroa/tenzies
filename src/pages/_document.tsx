5301521aad8e6145be0a4479c4337cde54d8400b
millan-figueroa <millan.fig@gmail.com>
2025-03-20 12:48:33 -0700

    Adds head element to import custom fonts

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,24 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        {/* Preconnect to Google Fonts */}
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="anonymous"
+        />
+
+        {/* Karla Font */}
+        <link
+          href="https://fonts.googleapis.com/css2?family=Karla:ital,wght@0,200..800;1,200..800&display=swap"
+          rel="stylesheet"
+        />
+
+        {/* Custom CSS */}
+        <link rel="stylesheet" href="/index.css" />
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
