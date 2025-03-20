709dcde8dce3a59a5448c8c305e5e578b11b624f
millan-figueroa <millan.fig@gmail.com>
2025-03-20 00:07:27 -0700

    Puts body and * styles in global css file

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,14 @@
 import Main from "../components/Main";
 
+type DieProps = {
+  value: number;
+};
+
 export default function Home() {
   return (
-    <div className="flex flex-col align-middle items-center h-full bg-gray-950">
+    <div className="h-100% w-100% max-h-[500px] max-w-[500px] bg-gray-950">
       <h1 className="text-4xl font-bold mt-4 text-slate-200">Tenzies</h1>
-      <Main />
+      <Main value={0} />
     </div>
   );
 }
