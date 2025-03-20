d5fac1633389fd933128f37326c1f43b44effa4d
millan-figueroa <millan.fig@gmail.com>
2025-03-20 12:47:45 -0700

    Creates function to randomly generate an array of numbers, maps over array and passes elements to vlue attribute

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,24 +1,34 @@
+import React, { useState } from "react";
 import Die from "./Die";
 
 type DieProps = {
   value: number;
 };
 
-export default function Main({ value }: DieProps) {
+export default function Main() {
+  const [dice, setDice] = React.useState(generateAllNewDice());
+
+  function generateAllNewDice() {
+    let newDice = [];
+    for (let i = 0; i < 10; i++) {
+      newDice.push(Math.floor(Math.random() * 6));
+    }
+    return newDice;
+  }
+
+  const diceElements = dice.map((num) => <Die value={num} />);
+
   return (
     <main className="bg-slate-50 h-full flex flex-col justify-center items-center rounded-b-lg">
-      <div className="grid grid-cols-5 gap-5 p-10 ">
-        <Die value={1} />
-        <Die value={2} />
-        <Die value={3} />
-        <Die value={4} />
-        <Die value={5} />
-        <Die value={6} />
-        <Die value={1} />
-        <Die value={2} />
-        <Die value={3} />
-        <Die value={4} />
+      <div className="grid grid-cols-5 gap-5 px-10 py-30 font-[Karla]">
+        {diceElements}
       </div>
+      <button
+        className="bg-blue-600 text-white p-5"
+        onClick={generateAllNewDice}
+      >
+        Roll
+      </button>
     </main>
   );
 }
