1610270a270b8dc08a1a64de7445065847eed002
millan-figueroa <millan.fig@gmail.com>
2025-03-22 10:44:43 -0700

    Adds styles to roll dice button

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -23,12 +23,12 @@ export default function Main() {
   const diceElements = dice.map((num) => <Die value={num} />);
 
   return (
-    <main className="bg-gray-100 h-full flex flex-col justify-center items-center rounded-b-lg">
+    <main className="bg-slate-200 h-full flex flex-col justify-center items-center rounded-b-lg">
       <div className="grid grid-cols-5 gap-5 px-10 py-15 font-[Karla]">
         {diceElements}
       </div>
       <button
-        className="p-5 mb-5 bg-gradient-to-bl from-violet-500 to-fuchsia-500 hover:from-violet-700 hover:to-fuchsia-700 active:scale-95 transition-all duration-200 text-white rounded-lg font-bold text-2xl"
+        className="px-15 py-4 mb-5 bg-gradient-to-bl from-violet-500 to-fuchsia-500 hover:from-violet-700 hover:to-fuchsia-700 active:scale-95 transition-all duration-200 text-white rounded-lg font-bold text-2xl"
         onClick={rollDice}
       >
         Roll
