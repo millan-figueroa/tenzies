87a48bfa0fd7508edd8d6cc2fc34cde9c163450f
millan-figueroa <millan.fig@gmail.com>
2025-03-29 16:11:48 -0700

    Refines layout of Home component for improved responsiveness; adjusts global styles for better spacing and alignment

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,8 +2,8 @@ import Main from "../components/Main";
 
 export default function Home() {
   return (
-    <div className="h-100% w-100% max-h-[500px] max-w-[500px] pt-2 bg-gray-900 rounded-t-lg">
-      <div className="mx-4 my-4 p-2 rounded-lg text-center text-slate-200 border-2 border-slate-200">
+    <div className="h-full w-100% max-h-[500px] max-w-[500px] pt-2 bg-gray-900 rounded-t-lg">
+      <div className="mx-4 my-4 p-2 rounded-lg text-center text-slate-200 border-1 border-slate-200">
         <h1 className="text-4xl py-2 font-bold ">Tenzies</h1>
         <p className="text-sm p-4 font-[Karla]">
           The goal is to roll all ten dice to the same number. Click/ tap on a
